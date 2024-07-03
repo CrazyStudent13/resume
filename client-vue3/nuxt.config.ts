a9f19fc214a30ce7c5f6abb19741e867ee34d580
crazystudent13 <1076535690@qq.com>
2024-07-04 00:17:31 +0800

    feat: 修改配置简介

--- client-vue3/nuxt.config.ts
+++ client-vue3/nuxt.config.ts
@@ -3,12 +3,12 @@ export default defineNuxtConfig({
   app: {
     // head
     head: {
-      title: 'Element Plus + Nuxt 3',
+      title: '个人简历',
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
           hid: 'description',
-          name: 'description',
+          name: '个人简历的描述',
           content: 'ElementPlus + Nuxt3',
         },
       ],
