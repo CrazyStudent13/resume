1830a42703278ec2da77500a9f78439017a152b5
crazystudent13 <1076535690@qq.com>
2024-07-04 00:18:25 +0800

    feat: 拆分简历模块，完成不同模块的组件

---
+++ client-vue3/components/BaseInfo/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-descriptions :column="3" style="margin-top: 32px">
+    <template #title>
+      <h2>{{ baseInfo.jobname }} - {{ baseInfo.name }}</h2>
+    </template>
+
+    <el-descriptions-item label="性别">{{ baseInfo.sex }}</el-descriptions-item>
+    <el-descriptions-item label="年龄">{{ baseInfo.age }}岁</el-descriptions-item>
+    <el-descriptions-item label="工作经验">{{ baseInfo.jobAge }}年</el-descriptions-item>
+    <el-descriptions-item label="学历">{{ baseInfo.education }}</el-descriptions-item>
+    <el-descriptions-item label="学校">{{ baseInfo.school }}</el-descriptions-item>
+    <el-descriptions-item label="期望薪资">{{ baseInfo.minSalary }} ~ {{ baseInfo.maxSalary }}</el-descriptions-item>
+    <el-descriptions-item label="期望所在地">{{ baseInfo.expectLocation }}</el-descriptions-item>
+    <el-descriptions-item label="当前所在地">{{ baseInfo.currentLocation }}</el-descriptions-item>
+  </el-descriptions>
+</template>
+
+<script setup lang="tsx">
+// 基本信息
+const baseInfo = {
+  name: '叶远川',
+  jobname: '前端开发工程师',
+  sex: '男',
+  age: '29',
+  jobAge: '5',
+  tel: '18650885088',
+  email: 'crazystudent13@qq.com',
+  currentLocation: '苏州（相城区）',
+  school: '南京铁道职业技术学院',
+  expectLocation: '苏州（相城区优先）',
+  minSalary: '15k',
+  maxSalary: '17k',
+  education: '大专',
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ client-vue3/components/CompanyExp/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="cardInfo">
+    <h3>工作经历</h3>
+    <el-table :data="companyInfoList" style="width: 100%">
+    <el-table-column prop="date" label="起止时间" width="180" >
+        <template #default="scope">
+            {{  scope.row.startTime }} ~ {{ scope.row.endTime  }}
+        </template>
+    </el-table-column>
+    <el-table-column prop="base" label="工作地" width="180" />
+    <el-table-column prop="name" label="公司名称" />
+  </el-table>
+
+  </div>
+</template>
+
+<script setup lang="tsx">
+const companyInfoList = [
+  {
+    name: '中资科技(江苏)',
+    base: '苏州',
+    startTime: '2018.07',
+    endTime: '2023.02',
+  },
+  {
+    name: '双盛科技（小袋鼠徐州分部）',
+    base: '徐州',
+    startTime: '2024.06',
+    endTime: '至今',
+  },
+]
+</script>
+
+<style lang="scss" scoped>
+.companyInfo {
+  span {
+    width: 200px;
+  }
+  &-item {
+    margin-right: 16px;
+  }
+}
+</style>
