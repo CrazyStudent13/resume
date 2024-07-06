6877ccb18111660cccbc2ee244179d87a4ff4038
crazystudent13 <1076535690@qq.com>
2024-07-07 00:27:31 +0800

    feat: 添加loading效果

--- client-vue3/app.vue
+++ client-vue3/app.vue
@@ -1,6 +1,22 @@
-<script setup>
-</script>
-
 <template>
-  <NuxtPage />
+  <!-- 首页加载全屏动画 -->
+  <FullLoading v-if="isFullLoading" />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const nuxtApp = useNuxtApp()
+
+// 是否首次加载
+const isFullLoading = ref(true)
+
+nuxtApp.hook('page:start', () => {
+  isFullLoading.value = true
+})
+
+nuxtApp.hook('page:finish', () => {
+  isFullLoading.value = false
+})
+</script>
